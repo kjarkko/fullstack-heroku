0e21bcc016583b8fc6381fe3ca5fb37590ecbcbf
Jarkko Karttunen <jarkko.karttunen1@gmail.com>
2019-02-11 19:07:13 +0200

    awegaewggwa

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.get('/api/persons/:id', (req, res) => {
 })
 app.delete('/api/persons/:id', (req,res) => {
   const id = Number(req.params.id)
-  Person.findByIdAndRemove(id)
+  Person.findByIdAndDelete(id)
   res.status(204).end()
 })
 app.post('/api/persons/', (req,res) => {
