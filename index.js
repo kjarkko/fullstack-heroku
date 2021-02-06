af42753eb70e4478d919a2390962c1b4024bd8af
Jarkko Karttunen <jarkko.karttunen1@gmail.com>
2021-02-06 21:28:26 +0200

    3c

--- index.js
+++ index.js
@@ -1,92 +1,117 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
+require('dotenv').config()
 
 const api = express()
 const port = process.env.PORT || 3001
 
 morgan.token('data', req => JSON.stringify(req.body))
 
-api.use(cors())
-api.use(express.json())
-api.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
-api.use(express.static('build'))
-
-let persons = [
-    {
-        id: 1,
-        name:'asdf',
-        number: '1234'
-    },{
-        id: 2,
-        name:'abc',
-        number: '9876'
-    },{
-        id: 3,
-        name:'xyz',
-        number: '0000'
-    }
-]
-
-const generateId = () => {
-    return Math.floor(Math.random() * (10000000000))
-}
-
 const validate = (person) => {
-    if (!person.name) {
+    if (!person) {
+        return "content is missing"
+    } else if (!person.name) {
         return "name is empty"
     } else if (!person.number) {
         return "number is empty"
-    } else if (persons.find(other => person.name === other.name)) {
-        return "name must be unique"
     } else {
         return null
     }
 }
 
+const errorHandler = (err, req, res, next) => {
+    console.error(err.message)
+  
+    if (err.name === 'CastError') {
+      return res.status(400).send({ error: 'malformatted id' })
+    } else if (err.name === 'ValidationError') {
+        return res.status(400).json({ error: error.message })
+    }
+  
+    next(err)
+}
+
+api.use(cors())
+api.use(express.json())
+api.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
+api.use(express.static('build'))
+api.use(errorHandler)
+
+
 api.get('/info', (req, res) => {
-    const date = new Date()
-    res.send(`phonebook has info for ${persons.length} persons<br>${date}`)
+    Person.find({}).then(persons => {
+        const date = new Date()
+        res.send(`phonebook has info for ${persons.length} persons<br>${date}`)
+    }).catch(err => next(err))
 })
 
 api.get('/api', (req, res) => {
     res.send('<h1>asd</h1>')
 }) 
 
-api.get('/api/persons', (req, res) => {
-    res.send(persons)
+api.get('/api/persons', (req, res, next) => {
+    Person.find({}).then(persons => {
+        res.send(persons)
+    }).catch(err => next(err))
 })
 
-api.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    person = persons.find(person => person.id === id)
-    if (person) {
-        res.json(person)
-    } else {
-        res.status(404).end()
-    }
+api.get('/api/persons/:id', (req, res, next) => {
+    Person.findById(request.params.id).then(person => {
+        if (person) {
+            response.json(person)
+        } else {
+            res.status(404).json({error: 'could not find person'})
+        }
+    }).catch(err => next(err))
 })
 
-api.post('/api/persons', (req, res) => {
-    const person = req.body
-    const err = validate(person)
+api.post('/api/persons', (req, res, next) => {
+    const body = req.body
+    const err = validate(body)
     if (err) {
         res.status(400).json({error: err})
     } else {
-        const id = generateId()
-        person.id = id
-        persons = persons.concat(person)
-        res.json(person)
+        const person = new Person({
+          name: body.name,
+          number: body.number
+        })
+
+        person.save().then(saved => {
+            res.json(saved)
+        }).catch(err => next(err))
     }
 })
 
-api.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(person => person.id != id)
+api.put('/api/persons/:id', (req, res, next) => {
+    const body = req.body
+    const err = validate(body)
+    if (err) {
+        res.status(400).json({error: err})
+    } else {
+        const person = {
+          name: body.name,
+          number: body.number
+        }
+        Person.findByIdAndUpdate(req.params.id, person, {new: true}).then(result => {
+            res.json(result)
+        }).catch(err => next(err))
+    }
+})
 
-    res.status(204).end()
+api.delete('/api/persons/:id', (req, res, next) => {
+    Person.findByIdAndRemove(req.params.id).then(result => {
+        res.status(204).end()
+    }).catch(err => next(err))
 })
 
 api.listen(port, () => {
     console.log(`running on ${port}`)
 })
+
+const unknownEndpoint = (req, res) => {
+    res.status(404).send({ error: 'unknown endpoint' })
+}
+
+api.use(unknownEndpoint)
---
+++ mongo.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose')
+
+if ( process.argv.length<3 ) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://fullstack:${password}@cluster0-qytxr.mongodb.net/test?retryWrites=true`
+
+mongoose.connect(url, { 
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true 
+})
+
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const save = (name, number) => {
+  const person = new Person({
+    name: name,
+    number: number
+  })
+  person.save().then(response => {
+    console.log('saved!')
+    mongoose.connection.close()
+  }).catch(e => console.log(e))
+}
+
+const all = () => {
+  Person.find({}).then(res => {
+    res.forEach(p => console.log(p.name, p.number))
+    mongoose.connection.close()
+  }).catch(e => console.log(e))
+}
+
+const args = process.argv
+const len = args.length
+if(len === 3)
+  all()
+else if(len === 5)
+  save(args[3], args[4])
+else 
+  console.log(`invalid number of arguments, got ${len}, expected 3 or 5. argv: ${process.argv}`)
